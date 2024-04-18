049e4a7d989ffb39a454d7fc6ea35583b501ac29
Joel Wembo <joelwembo@outlook.com>
2024-04-19 04:14:36 +0800

    added features

--- deployments/run.sh
+++ deployments/run.sh
@@ -1,11 +1,3 @@
-DOCKER_SCAN_SUGGEST=false docker build -t prodxcloud:latest .
-docker run -p 80:80 --name react prodxcloud:latest
-
-## run ssl
-# docker-compose -f ./deployments/traefik/docker-compose-ssl.yml up -d
-# DOCKER_SCAN_SUGGEST=false docker build -t prodxcloud:latest .
-# docker-compose -f ./docker-compose-ssl.yml up -d
-
 docker build -t joelwembo/prodxcloud:latest .
 docker run -p 80:80 --name react joelwembo/prodxcloud:latest
 
