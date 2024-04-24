8269fba4f3d012cd774c9220a0d965aae26d3a27
Joel Wembo <joelwembo@outlook.com>
2024-04-25 05:37:10 +0800

    testing load balancer with nginx proxy reverse

--- Dockerfile
+++ Dockerfile
@@ -19,8 +19,8 @@ COPY ngnix.conf /etc/nginx/conf.d/default.conf
 # Copy the React app build files to the container
 COPY --from=build /app/build /usr/share/nginx/html
 # Expose port 80 for Nginx
-EXPOSE 80
+# EXPOSE 80
 EXPOSE 3000
 EXPOSE 3000/tcp
 # Start Nginx when the container starts
-CMD ["nginx", "-g", "daemon off;"]
+# CMD ["nginx", "-g", "daemon off;"]
--- run.sh
+++ run.sh
@@ -12,9 +12,9 @@ docker run -d -p 3000:80 --name reacta joelwembo/prodxcloud:latest
 # For SSL Certificate in your react application use following Script
 
 # docker-compose -f ./deployments/traefik/docker-compose-ssl.yml up -d
-# DOCKER_SCAN_SUGGEST=false docker build -t prodxcloud .
+# DOCKER_SCAN_SUGGEST=false docker build -t prodxcloud . --no-cache
 # sudo docker run --rm -it -p 3000:3000 prodxcloud
-# docker run -d -p 3000:3000 --name react prodxcloud
+# docker run -d -p 3000:3000 --name react1 prodxcloud
 # docker-compose -f ./docker-compose-ssl.yml up -d
 
 
